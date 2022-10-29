013416790d65a48294a993ad2d1b34fbb7097b38
ndarkhan9 <darkhan2123@gmail.com>
2022-10-29 17:05:29 +0200

    Passed test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let spl = string.split("");
+    let rev = spl.reverse();
+    let jn = rev.join("");
+    return jn;
 };
 
 // Do not edit below this line
