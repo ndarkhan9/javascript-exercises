b64be8e63be2621b6d33820342b4230ee0530447
ndarkhan9 <darkhan2123@gmail.com>
2022-10-30 15:08:48 +0100

    Passed test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  return Math.round((fah-32)*(5/9)*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  return Math.round(((cel*1.8)+32)*10)/10;
 };
 
 // Do not edit below this line
