5b4aebed6e2ed0ea4d967e52fa9ae4a8664a4cd6
ndarkhan9 <darkhan2123@gmail.com>
2022-10-30 14:43:16 +0100

    Passed test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let count = 0;
+    if(!Number.isInteger(a)||!Number.isFinite(b))
+    {
+        return "ERROR";
+    }
 
+    if (a<0 || b<0)
+    {
+        return "ERROR";
+    }
+
+    if (a<b)
+    {
+
+        for(let i=a; i<=b;i++)
+        {
+            count += i;
+        }
+    }
+
+    else if(a>b)
+    {
+        for(let i=b; i<=a;i++)
+        {
+            count += i;
+        }
+    }
+    return count;
 };
 
 // Do not edit below this line
