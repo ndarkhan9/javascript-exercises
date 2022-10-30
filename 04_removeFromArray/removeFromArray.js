88798fac8a6a7527f59dec689c3ad6cbe8556c06
ndarkhan9 <darkhan2123@gmail.com>
2022-10-30 14:27:49 +0100

    Passed test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    arr = args[0];
+    return arr.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
